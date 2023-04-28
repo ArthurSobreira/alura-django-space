41e3a2dde68574bca5bb54fe1179d7be6f3c1746
Arthur <tutusobreira@gmail.com>
2023-04-28 19:36:55 -0300

    Update views.py

--- gallery/views.py
+++ gallery/views.py
@@ -1,6 +1,13 @@
 from django.shortcuts import render
 
 
+data = {
+    1: {'nome': 'Nebulosa de Carina',
+        'legenda': 'webbtelescope.org / NASA / James Webb'}, 
+    2: {'nome': 'Galáxia NGC 1079',
+        'legenda': 'nasa.org / NASA / Hubble'}
+}
+
 def index(request):
     return render(request, 'gallery/index.html')
 
